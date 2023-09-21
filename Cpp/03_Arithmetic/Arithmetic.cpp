32abfd465b0422bfd798dd8086d442942c3acbb5
Aryan132004 <malikaryan132004@gmail.com>
2023-09-22 01:33:44 +0530

    add

---
+++ Cpp/03_Arithmetic/Arithmetic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number1 , number2;
+    int sum;
+    cout<<"Enter the 1st number: ";
+    cin>>number1;
+    cout<<"Enter the 1st number: ";
+    cin>>number2;
+    
+    sum = number1 + number2;
+    cout<<"Addition of the numbers : "<<sum<<endl;
+}
