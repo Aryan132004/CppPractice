81154cc8a72582b33d69cd608023dbe7765f8c6c
Aryan132004 <malikaryan132004@gmail.com>
2023-09-22 01:23:29 +0530

    Input Output

---
+++ Cpp/02_Input_Output/Input_Output.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+    
+    cout<<"Enter you number: ";
+    cin>>number;
+    
+    cout<<"The number you have give in the input is: "<<number<<endl;
+}
