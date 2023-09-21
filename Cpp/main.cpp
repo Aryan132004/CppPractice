b7d5a5c167315b33a586e14f24a22abf0f3dc183
Aryan132004 <malikaryan132004@gmail.com>
2023-09-21 23:51:23 +0530

    Initial Commit

---
+++ Cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cpp
+//
+//  Created by Aryan Malik on 21/09/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
