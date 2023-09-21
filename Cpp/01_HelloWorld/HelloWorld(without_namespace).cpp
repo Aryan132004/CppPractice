d34495bb32eb9c1ffc79f9430829e85f3a1c5c18
Aryan132004 <malikaryan132004@gmail.com>
2023-09-22 00:59:16 +0530

    This is Hello World Without namespace(22 sept 2023)

---
+++ Cpp/01_HelloWorld/HelloWorld(without_namespace).cpp
@@ -0,0 +1,12 @@
+//
+//  HelloWorld(without_namespace).cpp
+//  Cpp
+//
+//  Created by Aryan Malik on 22/09/2023.
+//
+
+#include <iostream>
+int main(){
+    std::cout<<"Hello World"<<std::endl;
+    return 0;
+}
