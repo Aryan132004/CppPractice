7a7313156d04555d6efabe26ab71515ca963e2b7
Aryan132004 <malikaryan132004@gmail.com>
2023-09-22 15:56:09 +0530

    Type Modifier

---
+++ Cpp/0.5_DataType/TypeModifiers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    signed int a;
+    cout<<"The size of Signed int is "<<sizeof(a)<<endl;
+    
+    unsigned int b;
+    cout<<"The size of Unsigned int is "<<sizeof(b)<<endl;
+    
+    short int c;
+    cout<<"The size of short int is "<<sizeof(c)<<endl;
+    
+    long int d;
+    cout<<"The size of long int is "<<sizeof(d)<<endl;
+}
