5ae32fa60ce1670fa9ba4ee514a482bd389be36c
Aryan132004 <malikaryan132004@gmail.com>
2023-09-22 16:31:42 +0530

    Just input and output

---
+++ Cpp/02_Input_Output/Q1_Sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter the 1st ammount :";
+    cin>>a ;
+    
+    int b;
+    cout<<"Enter the 2nd ammount :";
+    cin>>b ;
+    
+    int sum;
+    sum=a+b;
+    cout<<"Enter the sum :"<<sum<<endl;
+}
---
+++ Cpp/02_Input_Output/Q1_alt.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a ;
+    
+    int b;
+    cin>>b ;
+    
+    int sum;
+    sum=a+b;
+    cout<<"Enter the sum :"<<sum<<endl;
+}
