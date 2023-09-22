78685f70583ed812c0d6bbf7385eba2f6de7cfab
Aryan132004 <malikaryan132004@gmail.com>
2023-09-22 15:55:49 +0530

    Data Types

---
+++ Cpp/0.5_DataType/DataType.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    int a; //declaration
+    a = 12; //initialization
+    cout<<"size of int is "<<sizeof(a)<<endl;
+    
+    float b; //declaration
+    cout<<"Size of float is "<<sizeof(b)<<endl;
+    
+    double c;
+    cout<<"Size of double is "<<sizeof(c)<<endl;
+    
+    char d;
+    cout<<"Size of Char is "<<sizeof(d)<<endl;
+    
+    bool e;
+    cout<<"Size of boolean is "<<sizeof(e)<<endl;
+    
+    return 0;
+}
