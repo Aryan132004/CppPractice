37db230e66361933f4ff145a16171de4a890d86e
Aryan132004 <malikaryan132004@gmail.com>
2023-10-02 01:24:24 +0530

    done

--- Cpp/11_FUNCTION/QUESTION/Q1.cpp
+++ Cpp/11_FUNCTION/QUESTION/Q1.cpp
@@ -5,4 +5,25 @@
 //  Created by Aryan Malik on 02/10/2023.
 //
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int BTD(int num){
+    int dec=0;
+    int base=1;
+    while(num > 0){
+        int last_dij=num%10;
+        num=num/10;
+        dec+=last_dij*base;
+        base*=2;
+    }
+    return dec;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    int j=BTD(n);
+    cout<<j<<endl;
+}
