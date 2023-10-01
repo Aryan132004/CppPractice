4466fe0da32045a7bf8c3d3dabd893238baedf77
Aryan132004 <malikaryan132004@gmail.com>
2023-10-02 02:04:35 +0530

    Q2

--- Cpp/11_FUNCTION/QUESTION/Q2.cpp
+++ Cpp/11_FUNCTION/QUESTION/Q2.cpp
@@ -3,6 +3,26 @@
 //  Cpp
 //
 //  Created by Aryan Malik on 02/10/2023.
-//
+// octal to dec
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int OTD(int num){
+    int base=1;
+    int dec=0;
+    while(num>0){
+        int last_dig=num%10;
+        num/=10;
+        dec=dec+last_dig*base;
+        base=base*8;
+    }
+    return dec;
+}
+
+int main(){
+    int num;
+    cin>>num;
+    int dec = OTD(num);
+    cout<<dec<<endl;
+}
